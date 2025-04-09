27f811be4336e1ad0d1baa8613999dddc967ca1e
athrv1837 <atharva1837@gmail.com>
2025-04-09 15:01:01 +0530

    deleted redis service

--- src/main/java/com/tnp/tnpbackend/service/RedisService.java
+++
@@ -1,6 +0,0 @@
-package com.tnp.tnpbackend.service;
-
-public interface RedisService {
-    public <T> T get(String key, Class<T> entityClass);
-    public void set(String key, Object o, Long ttl);
-}
