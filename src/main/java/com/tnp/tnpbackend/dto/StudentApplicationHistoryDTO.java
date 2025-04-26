28a213100a7babf3777b65a71ee75b978e5b2344
athrv1837 <atharva1837@gmail.com>
2025-04-26 11:57:16 +0530

    Made changes to StudentApplicationDTO

--- src/main/java/com/tnp/tnpbackend/dto/StudentApplicationHistoryDTO.java
+++ src/main/java/com/tnp/tnpbackend/dto/StudentApplicationHistoryDTO.java
@@ -1,5 +1,9 @@
 package com.tnp.tnpbackend.dto;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+
 import lombok.Data;
 
 @Data
@@ -7,7 +11,18 @@ public class StudentApplicationHistoryDTO {
     private String relationId; // ID of the StudentRecruiterRelation
     private String recruiterId;
     private String companyName;
+    private String companyWebsite;
+    private String companyLogoUrl;
+    private String companyDescription;
     private String jobRole;
+    private String jobDescription;
+    private LocalDate deadline;
+    private String companyLocation;
+    private Map<String, String> criteria;
+    private boolean isNotified = false;
+    private String industryType;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
     private String status; // "APPLIED", "INTERVIEWED", "HIRED"
     private String appliedAt; // Could use LocalDateTime, but String for simplicity
 }
